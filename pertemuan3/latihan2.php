a5809fe6e945016628fe9649638af4e2a3d28cc6
yogaarmdhn <yogaarmdhn@gmail.com>
2021-02-24 11:18:42 +0700

    Tugas pertemuan 3

---
+++ pertemuan3/latihan2.php
@@ -0,0 +1,15 @@
+<?php
+// Pengkondisian / Percabngan
+// Yoga Bagja Ramadhan - 203040112
+// if else, if else if else, ternary, switch
+
+$x = 20;
+if ( $x < 20 ) {
+    echo "Benar";
+} else if( $x == 20 ) {
+    echo "Bingo!";
+} else {
+    echo "Salah";
+}
+
+?>
