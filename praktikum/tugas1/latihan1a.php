88ac5b6cf1191ef5efa180dd74e0b2d8a532be97
yogaarmdhn <yogaarmdhn@gmail.com>
2021-03-02 14:53:08 +0700

    menyelesaikan tugas praktikum pertemuan 1

---
+++ praktikum/tugas1/latihan1a.php
@@ -0,0 +1,15 @@
+<!-- 
+Nama : Yoga Bagja Ramadhan
+NRP : 203040112
+Shift Praktikum : Kamis pukul 08:00
+-->
+
+<?php
+
+for( $i = 1; $i <= 3; $i++ ) {
+    for( $j = 1; $j <= 3; $j++ ) {
+        echo "Ini perulangan ke ($i,$j) <br>";
+    }
+}
+
+?>
