6b07398c125e69d74887b1cefdfaffa37a62697a
Yoga Bagja Ramadhan <75232249+yogaarmdhn@users.noreply.github.com>
2021-03-18 00:26:51 +0700

    Add files via upload

---
+++ praktikum/tugas3/Latihan3a.php
@@ -0,0 +1,16 @@
+<!-- 
+Nama : Yoga Bagja Ramadhan
+NRP : 203040112
+Shift Praktikum : Kamis pukul 08:00
+-->
+
+<?php
+$kalimat = [
+    "ada",
+    "abel",
+    "men",
+    "pung",
+    "nilai"
+];
+echo "Array $kalimat[0]lah suatu vari$kalimat[1] yang dapat $kalimat[2]am$kalimat[3] banyak $kalimat[4]";
+?>
---
+++ praktikum/tugas3/Latihan3b.php
@@ -0,0 +1,44 @@
+<!-- 
+Nama : Yoga Bagja Ramadhan
+NRP : 203040112
+Shift Praktikum : Kamis pukul 08:00
+-->
+
+<?php 
+$pemainBola = [
+    "Mohammad Salah",
+    "Cristiano Ronaldo",
+    "Lionel Messi",
+    "Zlatan Ibrahimovic",
+    "Neymar Jr"
+];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan3b 203040029</title>
+</head>
+<body>
+    <h3>Daftar pemain bola terkenal</h3>
+    <ol>
+        <?php foreach($pemainBola as $pemain) : ?>
+            <li><?= $pemain ?></li>
+        <?php endforeach; ?>
+    </ol>
+
+    <?php
+    array_push($pemainBola,"Luca Modric","Sadio Mane");
+    sort($pemainBola);
+    ?>
+
+    <h3>Daftar pemain bola terkenal baru</h3>
+    <ol>
+        <?php foreach($pemainBola as $pemainBaru) : ?>
+            <li><?= $pemainBaru ?></li>
+        <?php endforeach; ?>
+    </ol>
+</body>
+</html>
