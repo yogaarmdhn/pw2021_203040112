b0a655c08f260a4a5a12d2790bb8b685f76ae2ff
Yoga Bagja Ramadhan <75232249+yogaarmdhn@users.noreply.github.com>
2021-04-13 19:14:40 +0700

    tugas pertemuan 5

---
+++ praktikum/tugas5/latihan5a/php/admin.php
@@ -0,0 +1,14 @@
+<!-- 
+Nama : Yoga Bagja Ramadhan
+NRP : 203040112
+Shift Praktikum : Kamis pukul 08:00
+-->
+
+<?php
+// menghubungkan dengan file php lainnya
+require 'functions.php';
+
+// melakukan query
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+?>
