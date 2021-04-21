f2aad41acbecc02b25645b60d2bfebfccbbc5bc1
Yoga Bagja Ramadhan <75232249+yogaarmdhn@users.noreply.github.com>
2021-04-21 22:06:19 +0700

    update praktikum 5

---
+++ praktikum/tugas5/latihan5e/index.php
@@ -0,0 +1,20 @@
+<!-- 
+Nama : Yoga Bagja Ramadhan
+NRP : 203040112
+Shift Praktikum : Kamis pukul 08:00
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 5E</title>
+     <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php
+    include 'php/admin.php';
+    ?>
+</body>
+</html>
---
+++ praktikum/tugas5/latihan5e/php/hapus.php
@@ -0,0 +1,23 @@
+<!-- 
+Nama : Yoga Bagja Ramadhan
+NRP : 203040112
+Shift Praktikum : Kamis pukul 08:00
+-->
+
+<?php
+
+require 'functions.php';
+$id = $_GET['id'];
+
+if (hapus($id) > 0) {
+    echo "<script>
+            alert('Data Berhasil dihapus!');
+            document.location.href = '../index.php';
+            </script>";
+} else {
+    echo "<script>
+            alert('Data Gagal dihapus!');
+            document.location.href = '../index.php';
+            </script>";
+}
+?>
